7e7e36772e01eb7c2f1a2155f379d904d1b14efb
Noah Clarkson <unprotesting.email@gmail.com>
2023-11-23 11:56:38 +1300

    Refine and Expand Documentation in error.rs
    This update to error.rs focuses on improving the documentation by achieving the following goals:
    
    1. **Increased Clarity and Context**: Each error type and variant now includes a more descriptive comment, explaining its purpose and context within the parser and proof modules. This change aims to make the code more understandable and accessible, especially for new contributors or users of the library.
    
    2. **Detailing Error Conditions**: Specific details about what each error represents are added. For instance, `InvalidExpression` now clearly states that it's for characters not matching valid ones in an expression, and `UnmatchedParentheses` provides details about the partially parsed expression and the bracket index. This level of detail aids in quicker diagnosis and resolution of issues.
    
    3. **Consistency in Documentation Style**: The overall style of documentation has been made consistent throughout the file. This uniformity in the presentation of information improves readability and comprehension.
    
    4. **Enhancing Developer Experience**: With more informative error messages and clearer documentation, the debugging and development process becomes more intuitive. This update is particularly beneficial for maintaining the codebase and for developers integrating or utilizing these custom errors in their work.
    
    In summary, this commit enhances the usability and maintainability of the `error.rs` file by providing comprehensive, clear, and detailed documentation for each custom error type.

--- src/error.rs
+++ src/error.rs
@@ -1,25 +1,50 @@
+// error.rs:
+//
+// This module defines custom error types used in the parser and proof modules.
+// These error types provide more detailed and context-specific error messages,
+// improving the debugging experience and user feedback.
+
 use crate::proof::SearchState;
 
+/// Represents errors that can occur during parsing of logical expressions.
 #[derive(Debug, thiserror::Error)]
 pub enum ParserError {
+    /// Error for characters that don't match valid characters in an expression.
+    /// The offending character is provided in the error message.
     #[error("'{0}' did not match one of the valid characters.")]
     InvalidExpression(char),
+
+    /// Error for situations where an expression is expected but none is provided.
     #[error("Empty expression")]
     EmptyExpression,
+
+    /// Error for missing left operand in a binary operation.
     #[error("Expected left operand")]
     ExpectedLeftOperand,
+
+    /// Error for missing expression after a negation '-' operator.
     #[error("Expected expression after '-'")]
     ExpectedExpressionAfterNegation,
+
+    /// Error for an invalid operator in the expression.
+    /// The invalid operator character is provided in the error message.
     #[error("Invalid operator: '{0}'")]
     InvalidOperator(char),
+
+    /// Error for unmatched parentheses in an expression.
+    /// The partially parsed expression and the bracket index are provided.
     #[error("Unmatched parentheses in expression: {0} at bracket {1}")]
     UnmatchedParentheses(String, usize),
 }
 
+/// Represents errors that can occur during the proof process.
 #[derive(Debug, thiserror::Error)]
 pub enum ProofError {
+    /// Wraps a `ParserError`, indicating an error during parsing.
     #[error("Parser error: {0}")]
     ParserError(#[from] ParserError),
+
+    /// Represents errors occurring during the search state of proof generation.
     #[error("Search error: {0}")]
     SearchError(SearchState),
 }
