9508d69de95acb1f9403687c0234fd01431c99f3
Noah Clarkson <unprotesting.email@gmail.com>
2023-11-23 11:58:45 +1300

    Enhance Documentation in Expression Enum and Implementations
    This update focuses on refining the documentation of the `Expression` enum and its associated implementations in the codebase. Key improvements include:
    
    1. **Descriptive Enum Variants**: Each variant of the `Expression` enum is now accompanied by a comment explaining its role and usage in representing logical expressions. This makes the enum's purpose and function clear, aiding in understanding and maintenance.
    
    2. **Clarified Display Trait Implementation**: The implementation of the `Display` trait is documented to indicate its purpose in providing a human-readable format for expression instances. This clarifies its role in debugging and displaying expressions.
    
    3. **Detailed `list_expressions` Method Documentation**: The `list_expressions` method now has detailed comments explaining its functionality, including its recursive nature and the purpose of deduplicating expressions. This enhances the readability and usability of the method, making it easier for future developers to understand and modify the code.
    
    Overall, these documentation enhancements aim to make the codebase more accessible and maintainable, especially for new contributors or those unfamiliar with the logic expressions domain.

--- src/expression.rs
+++ src/expression.rs
@@ -1,15 +1,28 @@
 use std::fmt::{self, Display};
 use std::rc::Rc;
 
+/// Represents logical expressions in abstract syntax tree (AST) form.
+/// Supports basic logical operations like AND, OR, IMPLIES, and NOT, as well as variables.
 #[derive(Debug, PartialEq, Clone)]
 pub enum Expression {
+    /// Logical AND operation with two child `Expression` nodes.
     And(Rc<Expression>, Rc<Expression>),
+
+    /// Logical OR operation with two child `Expression` nodes.
     Or(Rc<Expression>, Rc<Expression>),
+
+    /// Logical IMPLIES operation with two child `Expression` nodes.
     Implies(Rc<Expression>, Rc<Expression>),
+
+    /// Logical NOT operation with a single child `Expression` node.
     Not(Rc<Expression>),
+
+    /// Represents a variable in the logical expression, stored as a `String`.
     Var(String),
 }
 
+/// Implementation of the `Display` trait for the `Expression` enum.
+/// This allows for the pretty printing of `Expression` instances in a human-readable format.
 impl Display for Expression {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
@@ -23,32 +36,24 @@ impl Display for Expression {
 }
 
 impl Expression {
+    /// Extracts and lists all unique sub-expressions (including the current one) from this `Expression`.
+    /// It traverses the AST recursively to gather all expressions.
+    /// 
+    /// Returns a `Vec<Expression>` containing all unique expressions found.
     pub fn list_expressions(&self) -> Vec<Expression> {
-        // Get all the nested expressions in a list
         let mut expressions = Vec::new();
         match self {
-            Expression::And(left, right) => {
-                expressions.push(Expression::And(left.clone(), right.clone()));
+            Expression::And(left, right) | Expression::Or(left, right) | Expression::Implies(left, right) => {
+                expressions.push(self.clone());
                 expressions.extend(left.list_expressions());
                 expressions.extend(right.list_expressions());
-            }
-            Expression::Or(left, right) => {
-                expressions.push(Expression::Or(left.clone(), right.clone()));
-                expressions.extend(left.list_expressions());
-                expressions.extend(right.list_expressions());
-            }
-            Expression::Implies(left, right) => {
-                expressions.push(Expression::Implies(left.clone(), right.clone()));
-                expressions.extend(left.list_expressions());
-                expressions.extend(right.list_expressions());
-            }
+            },
             Expression::Not(expr) => {
-                expressions.push(Expression::Not(expr.clone()));
+                expressions.push(self.clone());
                 expressions.extend(expr.list_expressions());
-            }
+            },
             Expression::Var(_) => expressions.push(self.clone()),
         }
-        // Remove duplicates
         expressions.dedup();
         expressions
     }
